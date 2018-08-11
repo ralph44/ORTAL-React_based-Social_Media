b2b515feb1850a3072ebe74f7749e2273dd0e8a2
Lennard <lennard.gerdes@gmx.net>
2018-08-12 01:59:26 +0200

    Doing stuff

---
+++ src/AlterInputComponent/AlterInputComponent.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import BoxComponent from '../BoxComponent/BoxComponent.js';
+import './AlterInputComponent.css';
+
+
+
+
+export default class AlterInputComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {text: '', inputText: '', mode:'view'};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSave = this.handleSave.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({ inputText: e.target.value });
+    }
+
+    handleSave() {
+        this.setState({text: this.state.inputText, mode: 'view'});
+    }
+
+
+
+    renderBox() {
+        let button = [];
+        if(this.state.text !== ''){
+            var i;
+            for (i = 0; i < this.state.text; i++) {
+                button.push(
+                    <BoxComponent/>
+                )
+            }
+        } else {
+
+        }
+        return button;
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="divForAlterInput">
+                    <input className="inputForAlter"
+                        placeholder="Alter eingeben"
+                        onChange={this.handleChange}
+                        value={this.state.inputText}
+                    />
+                    <button className="buttonForInput" onClick={this.handleSave}>Okay</button>
+                </div>
+                <br/>
+                <div>
+                    {this.renderBox()}
+                </div>
+
+            </div>
+        );
+    }
+}
--- src/Mainpage/Mainpage.js
+++ src/Mainpage/Mainpage.js
@@ -2,71 +2,21 @@ import React, { Component } from 'react';
 import './Mainpage.css';
 import Navbar from '../NavbarComponent/NavbarComponent.js';
 import BoxComponent from '../BoxComponent/BoxComponent.js';
-import BigBoxComponent from '../BigBoxComponent/BigBoxComponent.js';
+import AlterInputComponent from '../AlterInputComponent/AlterInputComponent';
 
 
 
 
 export default class Mainpage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {text: '', inputText: '', mode:'view'};
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSave = this.handleSave.bind(this);
-    }
-
-    handleChange(e) {
-        this.setState({ inputText: e.target.value });
-    }
-
-    handleSave() {
-        this.setState({text: this.state.inputText, mode: 'view'});
-    }
-
-    renderInputField() {
-        let input;
-        input =
-            <div>
-            <p>
-                Alter:</p>
-                    <input
-                        onChange={this.handleChange}
-                        value={this.state.inputText} />
-        <button onClick={this.handleSave}>Save</button>
-            </div>
-
-        return input;
-    }
-
-    renderBox() {
-        let button = [];
-        if(this.state.text !== ''){
-            var i;
-            for (i = 0; i < this.state.text; i++) {
-                button.push(
-                    <BoxComponent/>
-                )
-            }
-        } else {
-
-        }
-        return button;
-    }
 
   render() {
     return (
     <div>
         <Navbar/>
-
         <header>
-            <div>Welcome to ORTAL</div>
+            <div className="headerForWelcome">Welcome to ORTAL</div>
         </header>
-        {this.renderInputField()}
-
-        <div>
-          {this.renderBox()}
-        </div>
+        <AlterInputComponent/>
 
     </div>
     );
