d4e328e3484af17804eecd6ab5511005b892c9a4
Lenny <lennyknoop@hotmail.de>
2018-08-13 21:31:26 +0200

    Redux base implemented. next step is to call from the store

---
+++ src/Store/reducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+    age: 20
+}
+
+const reducer = (state = initialState, action) => {
+    return state;
+}
+
+export default reducer;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
+
 import './index.css';
 import App from './App/App';
 import registerServiceWorker from './registerServiceWorker';
 import { Login } from './Login/Login';
+import reducer from './Store/reducer';
 
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-
-   <App />,document.getElementById('root')
-
+   <Provider store={store}><App /></Provider>,document.getElementById('root')
 );
 
 registerServiceWorker();
